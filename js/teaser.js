cacebdf818b8ecd2d16c840a3b7b6b9e280c0ba8
Edwin Phillips <ed@kubuntu1604>
2017-08-01 22:19:24 +0100

    Add hoursperweek callback to teaser form

--- js/teaser.js
+++ js/teaser.js
@@ -1,29 +1,35 @@
 jQuery(document).ajaxComplete(function(event, xhr, settings) {
-  if (settings.extraData._triggering_element_name == 'castleschool_quote_months') {
+  if (settings.extraData._triggering_element_name == 'month') {
     jQuery.getScript("//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit2");
   }
 });
 
-jQuery('#edit-castleschool-quote-iwanttostudy').on('change', function() {
+jQuery(document).ajaxComplete(function(event, xhr, settings) {
+  if (settings.extraData._triggering_element_name == 'iwanttostudy') {
+    jQuery.getScript("//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit2");
+  }
+});
+
+jQuery('#block-castleschool-castleschool-teaser .form-item-iwanttostudy').on('change', function() {
   updatequote();
 });
 
-jQuery('#edit-castleschool-quote-months').on('change', function() {
+jQuery('#block-castleschool-castleschool-teaser .form-item-month').on('change', function() {
   updatequote();
 });
 
-jQuery('select[name="castleschool_quote_day"]').on('change', function() {
+jQuery('#block-castleschool-castleschool-teaser select[name="day"]').on('change', function() {
   updatequote();
 });
 
-jQuery('#edit-castleschool-quote-weeks').on('change', function() {
+jQuery('#block-castleschool-castleschool-teaser .form-item-weeks').on('change', function() {
   updatequote();
 });
 
 function updatequote() {
   var bookingfee = 80;
-  var quote = (jQuery('#edit-castleschool-quote-weeks').val() * 250) + bookingfee;
-  var accomm = jQuery('#edit-castleschool-quote-weeks').val() * 150;
+  var quote = (jQuery('#block-castleschool-castleschool-teaser .form-item-weeks').val() * 250) + bookingfee;
+  var accomm = jQuery('#block-castleschool-castleschool-teaser .form-item-weeks').val() * 150;
   jQuery('.castle-quote-total').text('£' + quote);
   jQuery('.castle-quote-accomm span').text('£' + accomm);
   jQuery('input[name=quotetotal]').val(quote);
