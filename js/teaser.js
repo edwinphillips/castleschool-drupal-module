2dce0030ec561c526841884dc14ec60d14d0eb36
Edwin Phillips <ed@kubuntu1604>
2017-08-03 02:21:56 +0100

    Booking and workflow validation and AJAX updates

--- js/teaser.js
+++ js/teaser.js
@@ -1,36 +1,31 @@
-jQuery(document).ajaxComplete(function(event, xhr, settings) {
-  if (settings.extraData._triggering_element_name == 'month') {
-    jQuery.getScript("//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit2");
+(function ($) {
+  Drupal.behaviors.castleschool = {
+    attach: function (context, settings) {
+        $('select[name="hoursperweek"]').once(function(){
+            $('select[name="hoursperweek"]').on('change',function() {
+                updatequote();
+            });
+        });
+        $('select[name="day"]').once(function(){
+            $('select[name="day"]').on('change',function() {
+                updatequote();
+            });
+        });
+        $('select[name="weeks"]').once(function(){
+            $('select[name="weeks"]').on('change',function() {
+                updatequote();
+            });
+        });
+        updatequote();
+
+        function updatequote() {
+            var course = $('select[name="course"]').val();
+            var hoursperweek = $('select[name="hoursperweek"]').val();
+            var startdate = $('select[name="day"]').val();
+            var weeks = $('select[name="weeks"]').val();
+            $('.castle-quote-wrapper').load('castleschool/quote/' + course + '/' + hoursperweek + '/' + startdate + '/' + weeks);
+            $.getScript("//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit2");
+        }
+    }
   }
-});
-
-jQuery(document).ajaxComplete(function(event, xhr, settings) {
-  if (settings.extraData._triggering_element_name == 'iwanttostudy') {
-    jQuery.getScript("//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit2");
-  }
-});
-
-jQuery('#block-castleschool-castleschool-teaser .form-item-iwanttostudy').on('change', function() {
-  updatequote();
-});
-
-jQuery('#block-castleschool-castleschool-teaser .form-item-month').on('change', function() {
-  updatequote();
-});
-
-jQuery('#block-castleschool-castleschool-teaser select[name="day"]').on('change', function() {
-  updatequote();
-});
-
-jQuery('#block-castleschool-castleschool-teaser .form-item-weeks').on('change', function() {
-  updatequote();
-});
-
-function updatequote() {
-  var bookingfee = 80;
-  var quote = (jQuery('#block-castleschool-castleschool-teaser .form-item-weeks').val() * 250) + bookingfee;
-  var accomm = jQuery('#block-castleschool-castleschool-teaser .form-item-weeks').val() * 150;
-  jQuery('.castle-quote-total').text('£' + quote);
-  jQuery('.castle-quote-accomm span').text('£' + accomm);
-  jQuery('input[name=quotetotal]').val(quote);
-}
+})(jQuery);
